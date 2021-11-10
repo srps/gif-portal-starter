4adc564f6409e37b39c2a4f9ee4d225e79bc0f29
srps <a_k_a_bouncer@hotmail.com>
2021-11-10 23:25:27 +0000

    Added phantom Wallet connection functionality
    + Built dependencies
    + Temporary fix for webpack OpenSSL3 error
    + Personalized Twitter handle
    + Added Solana Phantom Wallet connect

--- src/App.js
+++ src/App.js
@@ -1,28 +1,93 @@
-import twitterLogo from './assets/twitter-logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import twitterLogo from "./assets/twitter-logo.svg";
+import "./App.css";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const MY_TWITTER_HANDLE = "serj_mig";
+const TWITTER_HANDLE = "_buildspace";
 
 const App = () => {
+  const buildLink = (handle) => `https://twitter.com/${handle}`;
+
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom Wallet");
+
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            `Connected with public key: ${response.publicKey.toString()}`
+          );
+          setWalletAddress(response.publicKey.toString());
+        }
+      } else {
+        alert("Solana object not found! Get a Phantom Wallet 👻");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      console.log("Phantom Wallet");
+
+      const response = await solana.connect();
+      console.log(
+        `Connected with public key: ${response.publicKey.toString()}`
+      );
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? "authed-container" : "container"}>
         <div className="header-container">
           <p className="header">🖼 GIF Portal</p>
           <p className="sub-text">
             View your GIF collection in the metaverse ✨
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
             className="footer-text"
-            href={TWITTER_LINK}
+            href={buildLink(TWITTER_HANDLE)}
             target="_blank"
             rel="noreferrer"
           >{`built on @${TWITTER_HANDLE}`}</a>
+          <a
+            className="footer-text"
+            href={buildLink(MY_TWITTER_HANDLE)}
+            target="_blank"
+            rel="noreferrer"
+          >{` by @${MY_TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
